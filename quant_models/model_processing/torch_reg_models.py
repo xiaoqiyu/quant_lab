959b63a87c1d3e9d479ffb910c78c5c6cc4784e8
xiaoqiyu <rpyxqi@gmail.com>
2019-12-13 18:15:56 +0800

    update torch reg model

--- quant_models/model_processing/torch_reg_models.py
+++ quant_models/model_processing/torch_reg_models.py
@@ -9,6 +9,9 @@ import numpy as np
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
+import uqer
+from uqer import DataAPI
+
 torch.manual_seed(1)
 # HYPER Parameters
 TIME_STEP = 10  # rnn time step/image height
@@ -18,6 +21,70 @@ DOENLOAD_MINST = False
 BATCH_SIZE = 10
 HIDDEN_SIZE = 32
 NUM_LAYER = 2
+uqer_client = uqer.Client(token="26356c6121e2766186977ec49253bf1ec4550ee901c983d9a9bff32f59e6a6fb")
+
+
+def get_features(security_id=u"300634.XSHE", date='20191122'):
+    ticker, exchange_cd = security_id.split('.')
+    df = DataAPI.MktTicksHistOneDayGet(securityID=security_id, date=date, startSecOffset="", endSecOffset="",
+                                       field=u"", pandas="1")
+    df_min = DataAPI.SHSZBarHistOneDayGet(tradeDate=date, exchangeCD=exchange_cd, ticker=ticker, unit="5",
+                                          startTime=u"", endTime=u"", field=u"", pandas="1")
+    datatimes = list(df['dataTime'])
+    total_vol = list(df['value'])[-1]
+    data_min = ['{0}:{1}'.format(item.split(':')[0], item.split(':')[1]) for item in datatimes]
+    df['dataMin'] = data_min
+    df['avgPrice'] = df['value'] / df['volume']
+    df['amplitude'] = (df['highPrice'] - df['lowPrice']) / df['lastPrice']
+    df['spread'] = df['askPrice1'] - df['bidPrice1']
+    df['openDiff'] = (df['openPrice'] - df['prevClosePrice']) / df['prevClosePrice']
+    df['trackError'] = (df['lastPrice'] - df['avgPrice']) / df['avgPrice']
+    df['askTrackError1'] = (df['askPrice1'] - df['avgPrice']) / df['avgPrice']
+    df['bidTrackError1'] = (df['bidPrice1'] - df['avgPrice']) / df['avgPrice']
+    df['totalAskVolume'] = df['askVolume1'] + df['askVolume2'] + df['askVolume3'] + df['askVolume4'] + df['askVolume5']
+    df['totalBidVolume'] = df['bidVolume1'] + df['bidVolume2'] + df['bidVolume3'] + df['bidVolume4'] + df['bidVolume5']
+    df['volumeImbalance1'] = (df['askVolume1'] - df['bidVolume1']) / (df['askVolume1'] + df['bidVolume1'])
+    df['volumeImbalanceTotal'] = (df['totalAskVolume'] - df['totalBidVolume']) / (
+            df['totalAskVolume'] + df['totalBidVolume'])
+    df['volumePerDeal'] = df['volume'] / df['deal']
+    df['volumeRatio'] = df['volume'] / total_vol
+    int(list(set(df['dataMin']))[0].split(':')[1]) % 5
+    print(df.shape)
+    print(df.columns)
+
+    min_vwap = list(df_min['vwap'])
+    min_vwap.insert(0, min_vwap[0])
+    df_min['ret'] = (df_min['vwap'] / min_vwap[:-1] - 1) * 100
+    dict_min_ret = dict(zip(df_min['barTime'], df_min['ret']))
+    # TODO add features of tick and min
+    columns = list(df.columns)
+    columns.remove('dataDate')
+    columns.remove('exchangeCD')
+    columns.remove('ticker')
+    # columns.remove('dataTime')
+    # columns.remove('dataMin')
+    df.sort_values(by='dataTime', ascending=True, inplace=True)
+    data_min = list(df['dataMin'])
+    df = df[columns]
+    rows = list(df.values)
+    train_x = []
+    _start, _end = 0, 0
+    n_row = len(rows)
+    for idx, val in enumerate(rows):
+        hh, mm = data_min[idx].split(':')
+        if int(hh) == 9 and int(mm) <= 30:
+            continue
+        _start = idx
+        if idx == n_row-1:
+            pass
+        else:
+            hh_, mm_ = data_min[idx+1].split(':')
+            if int(mm) % 5 == 0 and int(mm_) != int(mm):
+                print(data_min[_start], data_min[idx])
+                train_x.append(rows[_start: idx])
+                _start = idx
+    return train_x
+
 
 
 class RNN(torch.nn.Module):
@@ -48,13 +115,13 @@ def rnn_reg_training():
     h_state = None  # 要使用初始hidden state, 可以设成None
     plt.ion()
     plt.show()
-    for step in range(10):
+    for step in range(100):
         # FIXME generate random test case
         x_np = np.random.random(BATCH_SIZE * TIME_STEP * INPUT_SIZE).reshape(BATCH_SIZE, TIME_STEP, INPUT_SIZE)
         y_np = np.random.random(BATCH_SIZE).reshape(BATCH_SIZE, 1)
 
-        x = torch.from_numpy(x_np)
-        y = torch.from_numpy(y_np)
+        x = torch.from_numpy(x_np).float()
+        y = torch.from_numpy(y_np).float()
 
         prediction, outputs, h_state = rnn(x, h_state)  # rnn对于每一个step的prediction, 还有最后一个step的h_state
         h_state = h_state.data  # 要把h_state 重新包装一下才能放入下一个iteration,不然会报错
@@ -63,13 +130,14 @@ def rnn_reg_training():
         optimizer.zero_grad()  # clear gradients for this training step
         loss.backward()  # backprogation, compute gradients
         optimizer.step()  # apply gradients
-        # if step % 5 == 0:
-        #     plt.cla()
-        #     plt.scatter(range(x.shape[0]), y[:,0].data.numpy())
-        #     plt.plot(range(x.shape[0]), outputs[:, 0].data.numpy(), 'r-', lw=5)
-        #     plt.text(0.5, 0, 'loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
-        #     plt.pause(.5)
+        if step % 5 == 0:
+            plt.cla()
+            plt.scatter(range(x.shape[0]), y[:, 0].data.numpy())
+            plt.plot(range(x.shape[0]), outputs[:, 0].data.numpy(), 'r-', lw=5)
+            plt.text(0.5, 0, 'loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+            plt.pause(1.5)
 
 
 if __name__ == '__main__':
-    rnn_reg_training()
+    # rnn_reg_training()
+    get_features()
